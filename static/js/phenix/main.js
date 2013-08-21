b651d40a4a3951aebfe586c957d6d373a20c361c
sarike <sarike@timefly.cn>
2013-08-21 21:12:43 +0800

    TODO：添加入口文件、tool-bar

---
+++ static/js/phenix/main.js
@@ -0,0 +1,8 @@
+/**
+ * Created with PyCharm.
+ * User: Administrator
+ * Date: 13-8-21
+ * Time: 下午9:11
+ * To change this template use File | Settings | File Templates.
+ */
+
---
+++ static/js/phenix/src/templates/tool-bar.tpl
@@ -0,0 +1,5 @@
+<ul class="tools">
+    <li class="tool-item"></li>
+    <li class="tool-item"></li>
+    <li class="tool-item"></li>
+</ul>
